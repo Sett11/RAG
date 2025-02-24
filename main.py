1378afb5703976fc9c2de03124d9dbe5a6522faf
Jhonny <gonzodzen@mail.ru>
2025-02-25 00:39:51 +0300

    App run

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import uvicorn as uvi
 
 app = FastAPI()
 
+
 @app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+async def root():
+    return {"message": "Hello World"}
+
+if __name__=='__main__':
+    uvi.run(app,host='127.0.0.1',port=8000)
