9769d9eb8c63037cd9d6bac5dd758f92165c2866
Jhonny <gonzodzen@mail.ru>
2025-03-26 00:00:41 +0300

    Add RAG

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-# import numpy as np
-# import faiss
+from tqdm import tqdm
+from math import sqrt
 
-# d = 64                           # dimension
-# nb = 100000                      # database size
-# nq = 10000                       # nb of queries
-# np.random.seed(1234)             # make reproducible
-# xb = np.random.random((nb, d)).astype('float32')
-# xb[:, 0] += np.arange(nb) / 1000.
-# xq = np.random.random((nq, d)).astype('float32')
-# xq[:, 0] += np.arange(nq) / 1000.
+t=0
 
-# index=faiss.IndexFlatL2(d)
-# print(index.is_trained)
-# index.add(xb)
-# print(index.ntotal)
+for i in tqdm(range(1000000)):
+    t+=sqrt(i)
 
-# k = 4                          # we want to see 4 nearest neighbors
-# D, I = index.search(xb[:5], k) # sanity check
-# print(I)
-# print(D)
-# D, I = index.search(xq, k)     # actual search
-# print(I[:5])                   # neighbors of the 5 first queries
-# print(I[-5:])                  # neighbors of the 5 last queries
-
-import os
-iskaggle = os.environ.get('KAGGLE_KERNEL_RUN_TYPE')
-
-print(iskaggle)
+print(t)
